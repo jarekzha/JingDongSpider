bc0e1ae736ad67e47663b40ae1d47b5b3d946bf9
jarekzha <jarekzha@gmail.com>
2016-04-11 22:56:16 +0800

    日志中增加时间字段

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -10,6 +10,7 @@ import config
 from jdItemPage import JDItemPage
 from mysqlSaver import MysqlSaver
 import codecs
+import time
   
 if __name__ == '__main__':
     ''' 解析页面 '''
@@ -18,7 +19,7 @@ if __name__ == '__main__':
     
     ''' 写日志 '''
     with codecs.open(config.LOG_FILE, 'a', 'utf-8') as logFile:
-        infoStr = u'itemID:%s, itemName:%s, price:%s, inStock:%s \n' % (itemPage.itemID, 
+        infoStr = u'%s itemID:%s, itemName:%s, price:%s, inStock:%s \n' % (time.ctime(), itemPage.itemID, 
             itemPage.itemName, itemPage.price, itemPage.inStock)
         logFile.write(infoStr)
     
